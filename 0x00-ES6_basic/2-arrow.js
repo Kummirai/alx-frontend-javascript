c6b9cf18c518bb3122aa244ba6e84022d4b6d61b
Milton <ajaxmilton@hotmail.com>
2024-07-22 18:44:52 +0200

    Double quote strings

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,7 +3,7 @@
 //Rewrite the following standard function to use ES6’s arrow syntax of the function add (it will be an anonymous function after)
 
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
   
     this.addNeighborhood = (newNeighborhood) => {
       this.sanFranciscoNeighborhoods.push(newNeighborhood);
