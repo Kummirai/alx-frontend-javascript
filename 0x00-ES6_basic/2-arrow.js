f405baabadfbf753761ca9b844c5560fc19a6a2e
Milton <ajaxmilton@hotmail.com>
2024-07-22 19:11:22 +0200

    Fix code

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,12 +1,9 @@
-#!/usr/bin/node
-
-//Rewrite the following standard function to use ES6’s arrow syntax of the function add (it will be an anonymous function after)
-
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
-  
-    this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
 }
