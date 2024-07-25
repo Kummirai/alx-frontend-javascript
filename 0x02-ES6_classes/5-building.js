fe8651bb91e6cafb313104da74f6a0e6e4acad89
Kummirai <ajaxmilton@hotmail.com>
2024-07-25 21:53:03 +0000

    fix code

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    console.log(this.#evacuationWarningMessage())
+    this.sqft = sqft;
   }
 
   get sqft() {
-    return this._sqft;
+    return this.sqft;
   }
 
-  set sqft(sqft) {
-    this._sqft = sqft;
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
-
- #evacuationWarningMessage(){
-  return 'Hey There'
- }
 }
