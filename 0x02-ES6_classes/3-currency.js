b796a65f3f9c8aed1e008e95ccc4af1cc280c286
Kummirai <ajaxmilton@hotmail.com>
2024-07-25 21:03:33 +0000

    Fix codde

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,48 +1,26 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    } else if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    } else if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
     this._name = name;
-    this._length = length;
-    this._students = students;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
   }
 
   get name() {
     return this._name;
   }
 
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
-  }
-
   set name(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = name;
+    this._code = name;
   }
 
-  set length(length) {
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = length;
-  }
-
-  set students(students) {
-    if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = students;
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
